7bdedce57fdf617bb41025fc31d87a24040c70b1
Mark Szymanski <mark.szymanski@vodori.com>
2014-12-15 20:09:24 -0600

    Replace tabs with spaces

--- src/main/java/gov/dol/CalculateHeatIndex.java
+++ src/main/java/gov/dol/CalculateHeatIndex.java
@@ -12,18 +12,18 @@ import java.text.DecimalFormat;
 public class CalculateHeatIndex {
     public static final String RESULT_FORMAT = "#.#";
 
-	public static double heatIndexCal(double F, double rh) {
-		double Hindex;
+    public static double heatIndexCal(double F, double rh) {
+        double Hindex;
 
-	    Hindex = -42.379 + 2.04901523 * F + 10.14333127 * rh;
-	    Hindex = Hindex - 0.22475541 * F * rh - 6.83783 * Math.pow(10, -3) * F * F;
-	    Hindex = Hindex	- 5.481717 * Math.pow(10, -2) * rh * rh;
-	    Hindex = Hindex	+ 1.22874 * Math.pow(10, -3) * F * F * rh;
-	    Hindex = Hindex	+ 8.5282 * Math.pow(10, -4) * F * rh * rh;
-	    Hindex = Hindex	- 1.99 * Math.pow(10, -6) * F * F * rh * rh;
+        Hindex = -42.379 + 2.04901523 * F + 10.14333127 * rh;
+        Hindex = Hindex - 0.22475541 * F * rh - 6.83783 * Math.pow(10, -3) * F * F;
+        Hindex = Hindex - 5.481717 * Math.pow(10, -2) * rh * rh;
+        Hindex = Hindex + 1.22874 * Math.pow(10, -3) * F * F * rh;
+        Hindex = Hindex + 8.5282 * Math.pow(10, -4) * F * rh * rh;
+        Hindex = Hindex - 1.99 * Math.pow(10, -6) * F * F * rh * rh;
 
         DecimalFormat df = new DecimalFormat(RESULT_FORMAT);
         df.setRoundingMode(RoundingMode.HALF_UP);
         return Double.parseDouble(df.format(Hindex));
-	}
+    }
 }
